06962a4ad3ae47c8b537c101394856a63470a9f4
messeb <sebastian@entwickler.me>
2013-04-17 14:24:24 +0200

    TSP algoritm
    add nearest neighbor and double tree algorithm

---
+++ examples/algorithm/tsp/double_tree.rb
@@ -0,0 +1,35 @@
+require_relative '../../../lib/vo/value'
+require_relative '../../../lib/io/edgelistreader'
+require_relative '../../../lib/vo/graph'
+require_relative '../../../lib/algorithm/tsp/double_tree'
+
+reader = EdgeListReader.new :file => '../../files/K_10.txt'
+edgelist = reader.readWithEdgeCosts
+
+count = reader.verticesCount
+
+g = Graph.new
+
+0.upto(count-1) do |i|
+	g.add_vertice(:value=>i)
+end
+
+edgelist.edges.each do |hash|
+	# puts hash
+	g.add_edge :fromValue => hash[:from], :toValue => hash[:to], :costs => hash[:costs]
+	g.add_edge :fromValue => hash[:to], :toValue => hash[:from], :costs => hash[:costs]
+end
+
+db = DoubleTree.new :graph => g
+result = db.get_edges :start_node => g.vertices[0]
+
+costs = 0;
+result.each do |edge|
+	costs = costs + edge.costs.to_f
+end
+
+result.each do |edge|
+	printf "%d => %d (%f)\n", edge.fromNode.value, edge.toNode.value, edge.costs
+end
+
+printf "%d Edges, %.4f costs\n", result.size, costs
